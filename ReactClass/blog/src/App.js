3718f1db0830533cee87fdc06835a94325cbf011
Seung-Bum <sb910126@gmail.com>
2022-05-12 10:35:45 +0900

    useState Practice

--- ReactClass/blog/src/App.js
+++ ReactClass/blog/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
-  let posts = '강남 고기 맛집 맛집';
-  let style = { color : 'blue', fontSize : '30px'};
-  
-  function 함수() {
-    return 100 
-  }
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let [글제목2, 글제목변경2] = useState('여자 스커트 추천');
+  let [글제목3, 글제목변경3] = useState('남자 봄옷 추천');
 
   return (
     <div className="App">
       <div className="App">
         <div className="black-nav">
-          <div style={ style }>개발 Blog</div>
+          <div>개발 Blog</div>
         </div>
-        <h4> { posts } </h4>
+        <div className="list">
+          <h3> { 글제목 } </h3>
+          <p>2월 17일 발행</p>
+          <hr></hr>
+        </div>
+        <div className="list">
+          <h3> { 글제목2 } </h3>
+          <p>3월 17일 발행</p>
+          <hr></hr>
+        </div>
+        <div className="list">
+          <h3> { 글제목3 } </h3>
+          <p>2월 17일 발행</p>
+          <hr></hr>
+        </div>
       </div>
     </div>
   );
